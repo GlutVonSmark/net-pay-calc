1ef86ffeb619600453f29e24400a8a532bccc155
Piotr Juszczyk <piotr.juszczyk@ascentialedge.com>
2020-03-05 21:27:33 +0000

    replacing useFormik with Formik

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useFormik } from 'formik';
+import {  FieldArray, Formik } from 'formik';
 import { Container, Button } from '@material-ui/core';
 import { createMuiTheme } from '@material-ui/core/styles';
 import { ThemeProvider } from '@material-ui/styles';
@@ -7,6 +7,7 @@ import CloudUploadIcon from '@material-ui/icons/CloudUpload';
 import FormNumberField from './FormNumberField';
 import Results from './Results'
 import calculate_net_pay from './tax-calc';
+import { FriendList } from './example'
 
 import styled, { css } from 'styled-components';
 
@@ -35,91 +36,104 @@ const App: React.FC = () => {
         tax_credit: 275,
         property_tax: 0
     };
-    const formik = useFormik({
-        initialValues,
-        onSubmit: values => {
-            alert(
-                calculate_net_pay(
-                    values.salary! / 12,
-                    false,
-                    values.health_insurance,
-                    values.travel,
-                    values.property_tax,
-                    values.tax_credit
-                )
-            );
-        }
-    });
+
+    // const formik = useFormik({
+    //     initialValues,
+    //     onSubmit: values => {
+    //         alert(
+    //             calculate_net_pay(
+    //                 values.salary! / 12,
+    //                 false,
+    //                 values.health_insurance,
+    //                 values.travel,
+    //                 values.property_tax,
+    //                 values.tax_credit
+    //             )
+    //         );
+    //     }
+    // });
+    // TODO: handle Blur (what to do with it)
     return (
+
+
+
         <Container maxWidth='md' style={{ textAlign: 'center' }}>
-            <StyledHeader>
-                Using Formik, Material UI with Styled Components and Typescript
-                and Netlify
-            </StyledHeader>
-            <StyledForm onSubmit={formik.handleSubmit}>
-                <Container>
-                    <ThemeProvider theme={theme}>
-                        <FormNumberField
-                            value={formik.values.salary}
-                            name='salary'
-                            label='Annual salary'
-                            required
-                            handleChange={formik.handleChange}
-                        ></FormNumberField>
-                        <FormNumberField
-                            name='tax_credit'
-                            label='Tax credit'
-                            required
-                            handleChange={formik.handleChange}
-                            value={formik.values.tax_credit}
-                        />
-                        <FormNumberField
-                            name='travel'
-                            label='Tax saver ticket'
-                            handleChange={formik.handleChange}
-                            value={formik.values.travel}
-                        />
-                        {/* NOTE: add info that you can add this to your tax  credit (animated gif)*/}
+                
+                <StyledHeader>
+                    Using Formik, Material UI with Styled Components and Typescript
+                    and Netlify
+                </StyledHeader>
+            <Formik initialValues={initialValues} onSubmit={values => console.log(values)}>
+                {
+                    ({values, handleSubmit, handleChange}) => (
 
-                        <FormNumberField
-                            name='health_insurance'
-                            label='Health Insurance'
-                            handleChange={formik.handleChange}
-                            value={formik.values.health_insurance}
-                        />
-
-                        <FormNumberField
-                            name='property_tax'
-                            label='Land Property Tax (LPT)'
-                            handleChange={formik.handleChange}
-                            value={formik.values.property_tax}
-                        />
-                        <p>
-                            <Button
-                                variant='contained'
-                                type='submit'
-                                color='primary'
-                                startIcon={<CloudUploadIcon />}
-                            >
-                                Submito!
-                            </Button>
-                        </p>
-                    </ThemeProvider>
-                </Container>
-                <StyledDiv standOut>
-                        {/* {JSON.stringify(formik.values, null, 2)} */}
                     
-                    {formik.values.salary > 0 && 
-                        <Results 
-                            salary={formik.values.salary/12}
-                            health_insurance={formik.values.health_insurance}
-                            travel={formik.values.travel}
-                            tax_credit={formik.values.tax_credit}
-                            property_tax={formik.values.property_tax}
-                        />
-                    }
-                </StyledDiv>
-            </StyledForm>
+                    <StyledForm onSubmit={handleSubmit}>
+                    <Container>
+                        <ThemeProvider theme={theme}>
+                            <FormNumberField
+                                value={values.salary}
+                                name='salary'
+                                label='Annual salary'
+                                required
+                                handleChange={handleChange}
+                                ></FormNumberField>
+                            <FormNumberField
+                                name='tax_credit'
+                                label='Tax credit'
+                                required
+                                handleChange={handleChange}
+                                value={values.tax_credit}
+                                />
+                            <FormNumberField
+                                name='travel'
+                                label='Tax saver ticket'
+                                handleChange={handleChange}
+                                value={values.travel}
+                                />
+                            {/* NOTE: add info that you can add this to your tax  credit (animated gif)*/}
+    
+                            <FormNumberField
+                                name='health_insurance'
+                                label='Health Insurance'
+                                handleChange={handleChange}
+                                value={values.health_insurance}
+                                />
+    
+                            <FormNumberField
+                                name='property_tax'
+                                label='Land Property Tax (LPT)'
+                                handleChange={handleChange}
+                                value={values.property_tax}
+                                />
+                            <p>
+                                <Button
+                                    variant='contained'
+                                    type='submit'
+                                    color='primary'
+                                    startIcon={<CloudUploadIcon />}
+                                    >
+                                    Submito!
+                                </Button>
+                            </p>
+                        </ThemeProvider>
+                    </Container>
+                    <StyledDiv standOut>
+                            {JSON.stringify(values, null, 2)}
+                        
+                        {values.salary > 0 && 
+                            <Results 
+                            salary={values.salary/12}
+                            health_insurance={values.health_insurance}
+                            travel={values.travel}
+                            tax_credit={values.tax_credit}
+                            property_tax={values.property_tax}
+                            />
+                        }
+                    </StyledDiv>
+                </StyledForm>
+                    )}
+            </Formik>
         </Container>
     );
 };
